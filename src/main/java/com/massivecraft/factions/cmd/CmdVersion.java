7e16b81d03229facc99f88069a2ad72daae48cb6
Tominous <18189490+TomCreeper@users.noreply.github.com>
2019-03-17 08:03:55 +0000

    Update CmdVersion.java

--- src/main/java/com/massivecraft/factions/cmd/CmdVersion.java
+++ src/main/java/com/massivecraft/factions/cmd/CmdVersion.java
@@ -8,8 +8,8 @@ public class CmdVersion extends FCommand {
     public CmdVersion() {
         this.aliases.add("version");
 
-        //this.requiredArgs.add("");
-        //this.optionalArgs.put("", "");
+        this.requiredArgs.add("");
+        this.optionalArgs.put("", "");
 
         this.permission = Permission.VERSION.node;
         this.disableOnLock = false;
