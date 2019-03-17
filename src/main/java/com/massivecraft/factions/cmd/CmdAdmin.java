8606c13720ef1a56ef6b9b64ef2c9c84e836541f
Tominous <18189490+TomCreeper@users.noreply.github.com>
2019-03-17 07:50:37 +0000

    Update CmdAdmin.java

--- src/main/java/com/massivecraft/factions/cmd/CmdAdmin.java
+++ src/main/java/com/massivecraft/factions/cmd/CmdAdmin.java
@@ -8,7 +8,7 @@ public class CmdAdmin extends FCommand {
         super();
         this.aliases.add("admin");
 
-        //this.requiredArgs.add("");
+        this.requiredArgs.add("");
         this.optionalArgs.put("on/off", "flip");
 
         this.permission = Permission.ADMIN.node;
@@ -25,11 +25,11 @@ public class CmdAdmin extends FCommand {
         fme.setHasAdminMode(this.argAsBool(0, !fme.hasAdminMode()));
 
         if (fme.hasAdminMode()) {
-            fme.msg("<i>You have enabled admin bypass mode.");
-            P.p.log(fme.getName() + " has ENABLED admin bypass mode.");
+            fme.msg("<i>You have enabled administrator bypass mode.");
+            P.p.log(fme.getName() + " has ENABLED administrator bypass mode.");
         } else {
-            fme.msg("<i>You have disabled admin bypass mode.");
-            P.p.log(fme.getName() + " DISABLED admin bypass mode.");
+            fme.msg("<i>You have disabled administrator bypass mode.");
+            P.p.log(fme.getName() + " DISABLED administrator bypass mode.");
         }
     }
 }
